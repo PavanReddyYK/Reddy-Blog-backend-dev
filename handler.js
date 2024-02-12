8d7583e3089049771e47e754fcac5ace29cef574
PavanReddyYk <pavanykreddy24@gmail.com>
2024-02-12 10:53:07 +0530

    created test and signUp api route

--- handler.js
+++ handler.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import userRoutes from './src/routes/auth.route.js';
+import authRoutes from './src/routes/user.route.js';
 
 import './dbConfig.js'
 const app = express();
 
-app.listen(3434, ()=>{
-    console.log(`server running on http://localhost:3434`);
-})
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
