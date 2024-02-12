667e0f65ae3dfb00e59de3367b2897bbad2a24f2
PavanReddyYk <pavanykreddy24@gmail.com>
2024-02-12 16:51:32 +0530

    added signin api route and ome changes

--- handler.js
+++ handler.js
@@ -2,23 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-import userRoutes from './src/routes/auth.route.js';
-import authRoutes from './src/routes/user.route.js';
+import authRoutes from "./src/routes/auth.route.js";
+import userRoutes from "./src/routes/user.route.js";
 
-import './dbConfig.js'
+import "./dbConfig.js";
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
+
+app.listen(process.env.PORT || 3434, () => {
+  console.log(`server running on http://localhost:3434`);
+});
